3538fa1908da4a520f7aa415e354d0d7597d74d0
Marc Bodmer <marc2422@gmail.com>
2012-02-10 14:44:00 -0500

    Updated source code

--- SimpleBreakout/HelloWorldLayer.h
+++ SimpleBreakout/HelloWorldLayer.h
@@ -12,16 +12,13 @@
 #import "Box2D.h"
 #import "GLES-Render.h"
 
-// HelloWorldLayer
-@interface HelloWorldLayer : CCLayer
-{
-	b2World* world;
-	GLESDebugDraw *m_debugDraw;
+@interface HelloWorldLayer : CCLayer {   
+    b2World *_world;
+    b2Body *_groundBody;
+    b2Fixture *_bottomFixture;
+    b2Fixture *_ballFixture;
 }
 
-// returns a CCScene that contains the HelloWorldLayer as the only child
-+(CCScene *) scene;
-// adds a new sprite at a given coordinate
--(void) addNewSpriteWithCoords:(CGPoint)p;
++ (id) scene;
 
 @end
